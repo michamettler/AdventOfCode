1a4a9d55cd39645cfac37620d48a28e19f0438b4
mettlmi1 <mettlermicha@gmail.com>
2024-12-05 09:59:24 +0100

    day 5 part two

--- 2024/micha/day5/solution2.py
+++ 2024/micha/day5/solution2.py
@@ -1,4 +1,37 @@
-with open('2024/micha/day5/input.txt') as file:
+with open('2024/micha/day5/testInput.txt') as file:
     locations = file.readlines()
-    
-# wip
+
+# part two - not very efficient, but who cares :-)
+
+def isValid(rules, pr):
+    for i, page in enumerate(pr):
+        if page in rules:
+            for toCheck in rules[page]:
+                if toCheck in pr and not pr.index(toCheck) > i:
+                    return False, i, pr.index(toCheck)
+    return True, -1, -1
+
+
+rules = {}
+prints = []
+
+for line in locations:
+    line = line.strip()
+    if ('|' in line):
+        v = line.split('|')
+        if v[0] not in rules:
+            rules[v[0]] = [v[1]]
+        else:
+            rules[v[0]].append(v[1])
+    elif ',' in line:
+        prints.append(list(line.split(',')))
+
+cntr = 0
+for pr in prints:
+    valid, a, b = isValid(rules, pr)
+    if not valid:
+        while not valid:
+            pr[b], pr[a] = pr[a], pr[b]
+            valid, a, b = isValid(rules, pr)
+        cntr += int(pr[len(pr)//2])
+print(cntr)
