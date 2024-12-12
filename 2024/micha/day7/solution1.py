435bb33f98e93360427f22722e0caaefb6fdcfba
micha mettler <micha.mettler@zkb.ch>
2024-12-12 16:00:04 +0100

    day 7

--- 2024/micha/day7/solution1.py
+++ 2024/micha/day7/solution1.py
@@ -1,2 +1,30 @@
-with open("2024/micha/day7/testInput.txt") as file:
-    locations = file.readlines()
+with open("2024/micha/day7/input.txt") as file:
+    lines = file.readlines()
+
+operations = []
+
+for line in lines:
+    split = list(line.split(':'))
+    operands = [int(item.strip()) for item in split[1].split(' ') if item]
+    operations.append([int(split[0]), operands])
+
+cntr = 0
+for operation in operations:
+    result = operation[0]
+    operands = operation[1]
+   
+    treeLevel = [operands[0]]
+    treeLevelNext = []
+    for i in range(1, len(operands)):
+        for node in treeLevel:
+            nodeLeft = node + operands[i]
+            nodeRight = node * operands[i]
+            
+            treeLevelNext.append(nodeRight)
+            treeLevelNext.append(nodeLeft)
+        treeLevel = treeLevelNext
+        treeLevelNext = []
+    if result in treeLevel:
+        cntr += result
+
+print(cntr)                    
--- 2024/micha/day7/solution2.py
+++ 2024/micha/day7/solution2.py
@@ -1,2 +1,32 @@
-with open("2024/micha/day7/testInput.txt") as file:
-    locations = file.readlines()
+with open("2024/micha/day7/input.txt") as file:
+    lines = file.readlines()
+
+operations = []
+
+for line in lines:
+    split = list(line.split(':'))
+    operands = [int(item.strip()) for item in split[1].split(' ') if item]
+    operations.append([int(split[0]), operands])
+
+cntr = 0
+for operation in operations:
+    result = operation[0]
+    operands = operation[1]
+   
+    treeLevel = [operands[0]]
+    treeLevelNext = []
+    for i in range(1, len(operands)):
+        for node in treeLevel:
+            node1 = node + operands[i]
+            node2 = node * operands[i]
+            node3 = int(str(node) + str(operands[i]))
+            
+            treeLevelNext.append(node1)
+            treeLevelNext.append(node2)
+            treeLevelNext.append(node3)
+        treeLevel = treeLevelNext
+        treeLevelNext = []
+    if result in treeLevel:
+        cntr += result
+
+print(cntr)                    
