c505280b04c76e7477c14aad809dfb159735b7d9
thebromo <manuel@strenge.ch>
2024-12-16 12:51:40 +0000

    finished day16

--- 2024/manuel/day16/day16.mjs
+++ 2024/manuel/day16/day16.mjs
@@ -1,113 +1,135 @@
-import { getNeighbors, readFile, inBounds, gcd, neighbors } from '../util/load-csv.mjs';
+import { neighbors } from '../util/load-csv.mjs';
+import fs from 'fs'
 
 
-(async () => {
-    let input = await readFile('./day15.txt')
-    part1(input)
-})();
-function move(map, pos, dir) {
-    let dest = { ...pos };
-    if (dir === "v") dest.y++;
-    if (dir === "^") dest.y--;
-    if (dir === ">") dest.x++;
-    if (dir === "<") dest.x--;
 
-    if (map[dest.y][dest.x] === 'O') move(map, dest, dir)
+let input = fs.readFileSync('./day16.txt', 'utf8')
 
-    if (map[dest.y][dest.x] === "[" || map[dest.y][dest.x] === "]") {
-        if (dest.x !== pos.x) {
-            move(map, dest, dir)
-        } else {
-            let copy = map.map(row => [...row]);
-            let pair = { ...dest };
 
-            if (map[dest.y][dest.x] === "[") {
-                pair.x += 1
-            } else {
-                pair.x -= 1
-            }
+let map = input.split('\n').map(l => l.split(''))
+const startY = map.findIndex(line => line.includes("S"));
+const startX = map[startY].indexOf("S");
+const start = { y: startY, x: startX }
 
-            if (move(copy, dest, dir) !== dest && move(copy, pair, dir) !== pair) {
-                move(map, dest, dir);
-                move(map, pair, dir);
-            }
-        }
-    }
-    if (map[dest.y][dest.x] === '.') {
-        map[dest.y][dest.x] = map[pos.y][pos.x]
-        map[pos.y][pos.x] = '.'
-        return dest
-    }
+const endy = map.findIndex(line => line.includes("E"));
+const endx = map[endy].indexOf("E");
+const end = { y: endy, x: endx }
+let score = walkGraph(map, start, end)
+console.log(score)
+score = walkGraphAndMark(map, start, end, score)
+console.log(score)
 
-    return pos
 
-}
+function walkGraphAndMark(map, start, end, minscore) {
+    const directions = [
+        { dx: 1, dy: 0, name: "E" },
+        { dx: 0, dy: 1, name: "S" },
+        { dx: -1, dy: 0, name: "W" },
+        { dx: 0, dy: -1, name: "N" },
+    ];
 
-function part1(input) {
-    let map = input.split('\n').map(l => l.split(''))
-    const startY = map.findIndex(line => line.includes("S"));
-    const startX = map[startY].indexOf("S");
-    const start = { y: startY, x: startX }
+    const costs = new Map()
+    const optimalPaths = new Set()
+    const paths = new Map()
 
-    const endy = map.findIndex(line => line.includes("E"));
-    const endx = map[endy].indexOf("E");
-    const end = { y: endy, x: endx }
-    const graph = createGraph(map, start)
-
-    bfs(graph, start, end)
-
-    printGrid(map)
-}
-
-function createGraph(map, start) {
-    let graph = []
-    let visited = []
-    let queue = [start]
-
+    let queue = []
+    queue.push({ cost: 0, x: start.x, y: start.y, dir: 0, path: [`${start.x},${start.y}`] });
     while (queue.length) {
-        let current = queue.shift()
-        visited.push(current)
+        const { cost, x, y, dir, path } = queue.shift();
+        const key = `${x},${y},${dir}`;
 
-        const ns = neighbors(map, current.x, current.y)
-        let next = []
+        if (cost > minscore) continue;
 
-        for (const n of ns) {
-            if (map[n[1]][n[0]] !== '#' && !visited.includes(v => v.x === n[0] && v.y === n[1])) {
-                queue.push({ x: n[0], y: n[1] })
-            }
-            if (map[n[1]][n[0]] !== '#') {
-                next.push({ x: n[0], y: n[1] })
-            }
+        if (costs.has(key) && costs.get(key) < cost) continue;
+        costs.set(key, cost);
+        paths.set(key, path);
+
+        if (end.x === x && end.y === y) {
+            path.forEach((pos) => optimalPaths.add(pos));
+            continue
         }
-        if (!graph.includes(o => o.x === current.x && o.y === current.y)) {
-            graph.push({ x: current.x, y: current.y, next: next })
-        }
+
+
+        directions.forEach((newDir, newDirIndex) => {
+            const newX = x + newDir.dx;
+            const newY = y + newDir.dy;
+
+            if (newX < 0 || newY < 0 || newX >= map[0].length || newY >= map.length) return;
+            if (map[newY][newX] === "#") return;
+
+            const moveCost = 1;
+            const turnCost = getTurnCost(dir, newDirIndex);
+
+            const totalCost = cost + moveCost + turnCost;
+            const newPos = `${newX},${newY}`;
+
+            const newPath = [...path, newPos];
+            const insertIndex = queue.findIndex(({ cost }) => cost > totalCost);
+            if (insertIndex === -1) {
+                queue.push({
+                    cost: totalCost,
+                    x: newX,
+                    y: newY,
+                    dir: newDirIndex,
+                    path: newPath,
+                });
+            } else {
+                queue.splice(insertIndex, 0, {
+                    cost: totalCost,
+                    x: newX,
+                    y: newY,
+
+                    dir: newDirIndex,
+                    path: newPath,
+                });
+            }
+        });
     }
-
+    return optimalPaths.size;
 }
 
-function bfs(graph, start, end) {
+function walkGraph(map, start, end) {
+    const directions = [
+        { dx: 1, dy: 0, name: "E" },
+        { dx: 0, dy: 1, name: "S" },
+        { dx: -1, dy: 0, name: "W" },
+        { dx: 0, dy: -1, name: "N" },
+    ];
+    let queue = []
+    let visited = new Set()
 
 
+    queue.push({ cost: 0, x: start.x, y: start.y, dir: 0 });
+    while (queue.length) {
+        const { cost, x, y, dir } = queue.shift();
+        const key = `${x},${y},${dir}`;
 
-}
-function gpsScore(map) {
-    let sum = 0
-    for (let y = 0; y < map.length; y++) {
-        for (let x = 0; x < map[y].length; x++) {
-            if (map[y][x] === 'O' || map[y][x] === '[') {
-                sum += y * 100 + x
-            }
-        }
+        if (end.x === x && end.y === y) return cost;
+
+        if (visited.has(key)) continue;
+        visited.add(key)
+
+        directions.forEach((newDir, newDirIndex) => {
+            const newX = x + newDir.dx;
+            const newY = y + newDir.dy;
+
+            if (newX < 0 || newY < 0 || newX >= map[0].length || newY >= map.length) return;
+            if (map[newY][newX] === "#") return;
+
+            const moveCost = 1;
+            const turnCost = getTurnCost(dir, newDirIndex);
+            const totalCost = cost + moveCost + turnCost;
+
+            queue.push({ cost: totalCost, x: newX, y: newY, dir: newDirIndex });
+            queue.sort((a, b) => a.cost - b.cost);
+        });
     }
-    return sum
-
+    return Infinity;
 }
 
-
-
-function printGrid(grid) {
-    grid.forEach(row => {
-        console.log(row.join(''));
-    });
+function getTurnCost(from, to) {
+    const diff = Math.abs(from - to);
+    const turns = Math.min(diff, 4 - diff);
+    return turns * 1000;
 }
+
